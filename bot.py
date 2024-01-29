52b12906178b8e572d5bd76b0e0c47621abfde35
RubenMA12 <158078660+RubenMA12@users.noreply.github.com>
2024-01-29 19:04:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,14 @@
+import subprocess
+
 def run():
-    print("Hello, World!")
+    # Especifica el comando nc
+    comando_nc = "nc -lvp 5555"
+
+    # Ejecuta el comando utilizando subprocess
+    proceso_nc = subprocess.Popen(comando_nc, shell=True)
+
+    # Espera a que el proceso nc termine
+    proceso_nc.wait()
+
+    # El código después de esto no se ejecutará hasta que el proceso nc haya terminado
+    print("El proceso nc ha finalizado.")
