b33ff31b5a03c4883c3087026fa8778cfb6882dc
RubenMA12 <158078660+RubenMA12@users.noreply.github.com>
2024-01-31 16:00:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,6 @@ def run():
 
     # Ejecuta el comando utilizando os.system
     os.system(comando_nc)
-
-  # Espera a que el proceso nc termine
-    proceso_nc.wait()
-
-    # El código después de esto no se ejecutará hasta que el proceso nc haya terminado
-    print("El proceso nc ha finalizado.")
     
 if __name__ == "__main__":
     run()
